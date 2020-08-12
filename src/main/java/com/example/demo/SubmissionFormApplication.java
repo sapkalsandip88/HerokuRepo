b882e59aaaa539a79a1e44ca6ceb73ca16e0dced
sandip sapkal <smdspksl28@gmail.com>
2020-08-12 14:57:21 +0530

    latest

--- src/main/java/com/example/demo/SubmissionFormApplication.java
+++ src/main/java/com/example/demo/SubmissionFormApplication.java
@@ -1,12 +1,15 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 @ComponentScan
 @SpringBootApplication
+@EnableAutoConfiguration(exclude=ErrorMvcAutoConfiguration.class)
 public class SubmissionFormApplication extends SpringBootServletInitializer{
 
 	
